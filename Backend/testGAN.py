9ff338f7b7248a370a2794f07d6d03be28d9c2a4
colin greeley <colin.greeley@wsu.edu>
2020-04-26 14:54:07 -0700

    fixed testGAN

--- Backend/testGAN.py
+++ Backend/testGAN.py
@@ -2,11 +2,25 @@ from __future__ import print_function, division
 
 import tensorflow as tf
 import matplotlib.pyplot as plt
+import tensorflow.keras as K
 import numpy as np
 import pandas as pd
 
-classifier = tf.keras.load_model('SLI_Model.h5')
-generator = tf.keras.load_model('generator')
+def make_model():
+    model = K.models.Sequential()
+    model.add(K.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)))
+    model.add(K.layers.MaxPooling2D((2, 2)))
+    model.add(K.layers.Conv2D(64, (3, 3), activation='relu'))
+    model.add(K.layers.MaxPooling2D((2, 2)))
+    model.add(K.layers.Conv2D(128, (3, 3), activation='relu'))
+    model.add(K.layers.Flatten())
+    model.add(K.layers.Dense(512, activation='relu'))
+    model.add(K.layers.Dense(25, activation='softmax'))
+    model.compile(optimizer='rmsprop', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+    model.summary()
+    return model
+
+generator = tf.keras.models.load_model('generator.h5')
 
 
 def generate_data(batch_size=1000, latent_dim=256):
@@ -19,12 +33,13 @@ def generate_data(batch_size=1000, latent_dim=256):
 
 if __name__ == "__main__":
     
-    models = [classifier for _ in range(10)]    # list of classifiers
+    models = [make_model() for _ in range(10)]    # list of classifiers
 
     for model in models:
-        x_train, y_train = generate_data()
-        model.fit(x_train, y_train)
+        x_train, y_train = generate_data(10000)
+        model.fit(x_train, y_train, epochs=2)
 
-    for model in models:
+    for i, model in enumerate(models):
         x_test, y_tset = generate_data(batch_size=100)
         model.evaluate(x_train, y_train)
+        model.save('classifier{}.h5'.format(i))
